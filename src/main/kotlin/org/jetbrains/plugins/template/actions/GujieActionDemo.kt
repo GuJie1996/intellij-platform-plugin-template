64c93db09ba86ce636e5e1f7ff9d38ac69230fb0
gujie <1045478997@qq.com>
2023-09-27 15:34:45 +0800

    添加按钮

---
+++ src/main/kotlin/org/jetbrains/plugins/template/actions/GujieActionDemo.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.template.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GujieActionDemo : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+
+}
